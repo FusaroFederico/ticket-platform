b44324febacc2b465668a7a9af6d806c269017d2
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-09-26 17:40:19 +0200

    Initial commit

---
+++ src/main/java/com/platform/ticket/spring/SpringTicketPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.platform.ticket.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTicketPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTicketPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/ticket/spring/SpringTicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.ticket.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
