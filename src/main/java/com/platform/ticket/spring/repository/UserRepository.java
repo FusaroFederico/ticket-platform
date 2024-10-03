7342756f0c1f5b8bbf84ac943bca03b847875c90
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-10-03 10:53:24 +0200

    Update UserRepository.java

--- src/main/java/com/platform/ticket/spring/repository/UserRepository.java
+++ src/main/java/com/platform/ticket/spring/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package com.platform.ticket.spring.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.platform.ticket.spring.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+	
+	//@Query(SELECT * 
+	//       FROM users
+	//       JOIN users_roles ON users.id = users_roles.user_id
+	//       JOIN role ON users_roles.role_id = role.id
+	//       WHERE role.name = "role";)
+	public List<User> findByRoles_Name(String role);
 }
