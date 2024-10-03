71a208f3566ed2e472ac1d1f3d447dd04c22f04e
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-10-03 14:19:33 +0200

    Update UserController.java
    Add updateIsActive method.

--- src/main/java/com/platform/ticket/spring/controller/UserController.java
+++ src/main/java/com/platform/ticket/spring/controller/UserController.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.platform.ticket.spring.model.Ticket;
+import com.platform.ticket.spring.model.User;
 import com.platform.ticket.spring.service.UserService;
 
 @Controller
@@ -20,7 +25,7 @@ public class UserController {
 	@GetMapping
 	public String index(Model model) {
 		
-		model.addAttribute("operators", userService.findAll());
+		model.addAttribute("operators", userService.getAllOperators());
 		return "/users/index";
 	}
 	
@@ -33,4 +38,25 @@ public class UserController {
 		return "/users/show";
 	}
 	
+	// Update isActive
+	@PostMapping("/updateIsActive/{id}")
+	public String updateIsActive(@PathVariable("id") Integer id,
+								 @RequestParam(name = "isActive", required = true) Boolean isActive,
+								 RedirectAttributes redirectAttributes) {
+		
+		User user = userService.getById(id);
+		
+		if (user.getTickets().size() > 0) {
+			for( Ticket ticket : user.getTickets()) {
+				if ( !ticket.getStatus().equals("completato") ) {
+					redirectAttributes.addFlashAttribute("non puoi aggiornare il tuo stato");
+					return "redirect:/users/" + user.getId();
+				}
+			}
+		}
+		
+		userService.updateIsActive(user, isActive);
+		redirectAttributes.addFlashAttribute("Stato aggiornato con successo.");
+		return "redirect:/users/" + user.getId();
+	}
 }
