068b19a5b2a47848fcf83c364a476396d0cfea51
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-10-05 19:20:05 +0200

    Add Entity Category with related Repository and Service

---
+++ src/main/java/com/platform/ticket/spring/model/Category.java
@@ -0,0 +1,57 @@
+package com.platform.ticket.spring.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotBlank
+	@Size(min = 3, max = 20)
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@ManyToMany(mappedBy = "categories")
+	private List<Ticket> tickets;
+
+	// Getters and Setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Ticket> getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(List<Ticket> tickets) {
+		this.tickets = tickets;
+	}
+	
+	
+}
---
+++ src/main/java/com/platform/ticket/spring/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.platform.ticket.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.platform.ticket.spring.model.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+}
