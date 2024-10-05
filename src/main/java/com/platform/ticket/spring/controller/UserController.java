b5767c2975fdc263f51c74eef753d9abc7950b92
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-10-05 19:47:54 +0200

    Update UserController.java
    Improved updateIsActive method.
    Create showUserProfile method.

--- src/main/java/com/platform/ticket/spring/controller/UserController.java
+++ src/main/java/com/platform/ticket/spring/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.platform.ticket.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.platform.ticket.spring.model.Ticket;
 import com.platform.ticket.spring.model.User;
+import com.platform.ticket.spring.security.DatabaseUserDetails;
 import com.platform.ticket.spring.service.UserService;
 
 @Controller
@@ -26,6 +29,7 @@ public class UserController {
 	public String index(Model model) {
 		
 		model.addAttribute("operators", userService.getAllOperators());
+		
 		return "/users/index";
 	}
 	
@@ -33,30 +37,45 @@ public class UserController {
 	@GetMapping("/{id}")
 	public String show(@PathVariable("id") Integer id, Model model) {
 		
-		
+		model.addAttribute("title", "Dettagli Operatore");
 		model.addAttribute("operator", userService.getById(id));
+		
 		return "/users/show";
 	}
 	
 	// Update isActive
-	@PostMapping("/updateIsActive/{id}")
-	public String updateIsActive(@PathVariable("id") Integer id,
+	@PostMapping("/updateIsActive")
+	public String updateIsActive(@AuthenticationPrincipal DatabaseUserDetails currentUser,
 								 @RequestParam(name = "isActive", required = true) Boolean isActive,
 								 RedirectAttributes redirectAttributes) {
 		
-		User user = userService.getById(id);
+		User user = userService.getById(currentUser.getId());
 		
-		if (user.getTickets().size() > 0) {
+		if (user.getTickets().size() > 0 && !isActive) {
 			for( Ticket ticket : user.getTickets()) {
 				if ( !ticket.getStatus().equals("completato") ) {
-					redirectAttributes.addFlashAttribute("non puoi aggiornare il tuo stato");
-					return "redirect:/users/" + user.getId();
+					redirectAttributes.addFlashAttribute("alertMessage", "Impossibile impostare lo stato su 'non attivo' se hai almeno un ticket con stato 'da fare' o 'in corso'!");
+					redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+					return "redirect:/users/profile";
 				}
 			}
 		}
 		
 		userService.updateIsActive(user, isActive);
-		redirectAttributes.addFlashAttribute("Stato aggiornato con successo.");
-		return "redirect:/users/" + user.getId();
+		redirectAttributes.addFlashAttribute("alertMessage", "Stato aggiornato con successo.");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		return "redirect:/users/profile";
+	}
+	
+	// Show User Profile
+	@GetMapping("/profile")
+	public String showUserProfile(@AuthenticationPrincipal DatabaseUserDetails currentUser, Model model) {
+		
+		model.addAttribute("title", "Profilo Utente");
+		model.addAttribute("operator", userService.getById(currentUser.getId()));
+		
+		return "/users/show";
+	}
+	
 	}
 }
